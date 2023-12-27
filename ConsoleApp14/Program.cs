056db28bbb5a95fe2f68563d5034b3a21fcb7a02
doveka <131176206+doveka@users.noreply.github.com>
2023-12-27 23:06:01 +0200

    Add files via upload

---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,19 @@
+﻿{
+    static void Main()
+    {
+        int[] arr = { 5, 2, 8, 1, 9, 3 };
+        
+        SortDescending(arr);
+        
+        foreach (int num in arr)
+        {
+            Console.Write(num + " ");
+        }
+    }
+    
+    static void SortDescending(int[] arr)
+    {
+        Array.Sort(arr);
+        Array.Reverse(arr);
+    }
+}
